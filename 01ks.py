0878b295cfef30e863adac0784effd12a3bc2b46
govind cs <govindsomalal7@gmail.com>
2017-10-11 22:22:11 +0530

    implemented DP

--- 01ks.py
+++ 01ks.py
@@ -1,9 +1,14 @@
 weight = [10, 20, 30]
 vals =[60,100,120] 
+memo = {}
 def ks(n, W ):
     if n == 0 or W == 0:
         return 0
+    elif (n,W) in memo:
+        return memo[n,W]
     else:
-        return max(ks(n-1,W), vals[n-1] + ks(n-1, W-weight[n-1]))
+        ans = max(ks(n-1,W), vals[n-1] + ks(n-1, W-weight[n-1]))
+        memo[n,W] = ans
+        return ans
 
 print(ks(3,50))
